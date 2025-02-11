e973ec614ee3770f87f2e2c796b4552921c40560
Ethan Dean <ethandean7@gmail.com>
2025-02-11 09:42:53 -0500

    Add basic home page and call to action

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
   return (
-    <div className='home-page'>
-      <p className="text-blue-400">This is the home page</p>
+    <div className='mt-5'>
+      <img className='mt-5 w-[80vw] bg-blue-400 rounded-md shadow-lg' src='combo-logo.png' />
+      <p className='mt-5 p-4 w-[80vw] bg-blue-400 rounded-md shadow-lg text-lg text-center text-white font-semibold'>Set sail on an unforgettable adventure by connecting with fellow cruisers before you even board! Join our community to find like-minded travelers, plan group excursions, and make new friends for your voyage. Don’t miss out—sign up now and turn your cruise into a social experience you’ll never forget!</p>
     </div>
   );
 }
